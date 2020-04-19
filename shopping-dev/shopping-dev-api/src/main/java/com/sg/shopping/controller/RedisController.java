c2b112a06921db9df97cfa046cd2ce14693db7ce
Shun Gao <theodore.gao.1986@gmail.com>
2020-04-19 15:01:32 -0700

    Add redis util class.

--- shopping-dev/shopping-dev-api/src/main/java/com/sg/shopping/controller/RedisController.java
+++ shopping-dev/shopping-dev-api/src/main/java/com/sg/shopping/controller/RedisController.java
@@ -1,5 +1,6 @@
 package com.sg.shopping.controller;
 
+import com.sg.shopping.common.utils.RedisOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,20 +17,26 @@ public class RedisController {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @Autowired
+    private RedisOperator redisOperator;
+
     @GetMapping("/set")
     public Object set(@RequestParam String key, @RequestParam String value) {
-        redisTemplate.opsForValue().set(key, value);
+//        redisTemplate.opsForValue().set(key, value);
+        redisOperator.set(key, value);
         return "OK";
     }
 
     @GetMapping("/get")
     public String get(@RequestParam String key) {
-        return (String) redisTemplate.opsForValue().get(key);
+//        return (String) redisTemplate.opsForValue().get(key);
+        return redisOperator.get(key);
     }
 
     @GetMapping("/delete")
     public String delete(@RequestParam String key) {
-        redisTemplate.delete(key);
+//        redisTemplate.delete(key);
+        redisOperator.del(key);
         return "OK";
     }
 
