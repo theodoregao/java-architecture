335d25992faddb915757dd1a725f5dedab1f9e26
Shun Gao <theodore.gao.1986@gmail.com>
2020-04-22 15:50:31 -0700

    Add interceptor for required request paths.

--- shopping-dev/shopping-dev-api/src/main/java/com/sg/shopping/config/WebMvcConfig.java
+++ shopping-dev/shopping-dev-api/src/main/java/com/sg/shopping/config/WebMvcConfig.java
@@ -39,7 +39,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(userTokenInterceptor())
-                .addPathPatterns("/hello");
+                .addPathPatterns("/hello")
+                .addPathPatterns("/shopcart/add")
+                .addPathPatterns("/shopcart/del")
+                .addPathPatterns("/address/list")
+                .addPathPatterns("/address/add")
+                .addPathPatterns("/address/update")
+                .addPathPatterns("/address/setDefalut")
+                .addPathPatterns("/address/delete")
+                .addPathPatterns("/orders/*")
+                .addPathPatterns("/center/*")
+                .addPathPatterns("/userInfo/*")
+                .addPathPatterns("/myorders/*")
+                .addPathPatterns("/mycomments/*")
+                .excludePathPatterns("/myorders/deliver")
+                .excludePathPatterns("/orders/notifyPaid");
         WebMvcConfigurer.super.addInterceptors(registry);
     }
 }
--- shopping-dev/shopping-dev-api/src/main/java/com/sg/shopping/interceptor/UserTokenInterceptor.java
+++ shopping-dev/shopping-dev-api/src/main/java/com/sg/shopping/interceptor/UserTokenInterceptor.java
@@ -1,5 +1,8 @@
 package com.sg.shopping.interceptor;
 
+import com.sg.shopping.common.utils.RedisOperator;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -7,10 +10,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class UserTokenInterceptor implements HandlerInterceptor {
+
+    public static final String REDIS_USER_TOKEN = "redis_user_token";
+
+    @Autowired
+    private RedisOperator redisOperator;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("Intercepted and rejected.");
-        return false;
+        String userId = request.getHeader("headerUserId");
+        String userToken = request.getHeader("headerUserToken");
+        if (StringUtils.isNotBlank(userId) && StringUtils.isNotBlank(userToken)) {
+            String uniqueToken = redisOperator.get(REDIS_USER_TOKEN + ":" + userId);
+            if (StringUtils.isBlank(uniqueToken)) {
+                System.out.println("Operation intercepted and user not login. Please login first");
+                return false;
+            } else {
+                if (!uniqueToken.equals(userToken)) {
+                    System.out.println("User was logged in a different place.");
+                    return false;
+                }
+            }
+        } else {
+            System.out.println("Operation intercepted and user not login. Please login first");
+            return false;
+        }
+        return true;
     }
 
     @Override
