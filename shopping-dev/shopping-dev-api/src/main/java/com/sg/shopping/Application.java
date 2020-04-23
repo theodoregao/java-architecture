d0398725a97bb00bf113d26a9db92c81e6678479
Shun Gao <theodore.gao.1986@gmail.com>
2020-04-22 17:24:53 -0700

    Disable spring redis session.

--- shopping-dev/shopping-dev-api/src/main/java/com/sg/shopping/Application.java
+++ shopping-dev/shopping-dev-api/src/main/java/com/sg/shopping/Application.java
@@ -2,17 +2,18 @@ package com.sg.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+//import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 @MapperScan(basePackages = "com.sg.shopping.mapper")
 @ComponentScan(basePackages = {"com.sg.shopping", "org.n3r.idworker"})
 @EnableScheduling
-@EnableRedisHttpSession // 开启使用redis作为spring session
+//@EnableRedisHttpSession // 开启使用redis作为spring session
 public class Application {
 
     public static void main(String[] args) {
