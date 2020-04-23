60ae076561a8f045667226b85d0ad5ce90536ef9
Shun Gao <theodore.gao.1986@gmail.com>
2020-04-22 17:43:50 -0700

    Return error message for response in interceptor.

--- shopping-dev/shopping-dev-api/src/main/java/com/sg/shopping/interceptor/UserTokenInterceptor.java
+++ shopping-dev/shopping-dev-api/src/main/java/com/sg/shopping/interceptor/UserTokenInterceptor.java
@@ -1,5 +1,7 @@
 package com.sg.shopping.interceptor;
 
+import com.sg.shopping.common.utils.JsonResult;
+import com.sg.shopping.common.utils.JsonUtils;
 import com.sg.shopping.common.utils.RedisOperator;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class UserTokenInterceptor implements HandlerInterceptor {
 
@@ -24,15 +28,18 @@ public class UserTokenInterceptor implements HandlerInterceptor {
             String uniqueToken = redisOperator.get(REDIS_USER_TOKEN + ":" + userId);
             if (StringUtils.isBlank(uniqueToken)) {
                 System.out.println("Operation intercepted and user not login. Please login first");
+                returnErrorResponse(response, JsonResult.errorException("Please login"));
                 return false;
             } else {
                 if (!uniqueToken.equals(userToken)) {
                     System.out.println("User was logged in a different place.");
+                    returnErrorResponse(response, JsonResult.errorException("User was logged in a different place."));
                     return false;
                 }
             }
         } else {
             System.out.println("Operation intercepted and user not login. Please login first");
+            returnErrorResponse(response, JsonResult.errorException("Please login"));
             return false;
         }
         return true;
@@ -46,5 +53,26 @@ public class UserTokenInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 
+    }
+
+    public void returnErrorResponse(HttpServletResponse response, JsonResult result) {
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/json");
+        OutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            outputStream.write(JsonUtils.objectToJson(result).getBytes("utf-8"));
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
